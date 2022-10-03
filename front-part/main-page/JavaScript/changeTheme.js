548f64449bb035d2969810d85058fdcb9192ff70
luiofff <95875204+luiofff@users.noreply.github.com>
2022-10-03 19:54:39 +0300

    themeChange

---
+++ front-part/main-page/JavaScript/changeTheme.js
@@ -0,0 +1,30 @@
+let state = "white";
+
+changeTheme.addEventListener("click", () => {
+    if (state==="white") {
+        // background
+        document.body.style.background = "linear-gradient(180deg, #000000 0%, #5C204C 0.02%, #FFFFFF 208.77%)";
+        // pair block back
+        pairBlockJs.style.background = "#460B36";
+        // change Theme
+        profileBlockJs.style.background = "#460B36";
+        // logo
+        changeColorLogoJs1.style.color = "#fff";
+        changeColorLogoJs2.style.color = "#fff";
+
+        state = "black";
+    } else {
+        // background
+        document.body.style.background = "linear-gradient(180deg, #FFFFFF 0%, #7D2063 224.44%)";
+        // pair block back
+        pairBlockJs.style.background = "#5D214D";
+        // change Theme
+        profileBlockJs.style.background = "#5D214D";
+        // logo
+        changeColorLogoJs1.style.color = "#111";
+        changeColorLogoJs2.style.color = "#111";
+
+
+        state = "white";
+    }
+})
